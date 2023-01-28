5ffd3dc955507f6860018239e2001b0a67fd6eb7
ahmadZaid <ahmaadzaid7@gmail.com>
2023-01-28 20:37:10 +0300

    change logo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,9 +1,21 @@
+<!-- <style>
+    #logo-header {
+        background-image: linear-gradient(to right, #FA5252, #DD2476);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-size: 24px;
+        font-weight: bold;
+    }
+</style> -->
+
 <div class="section-bg">
     <div class="w-full flex justify-between px-4">
         <!-- website Logo -->
-        <a href="/">
-            <img class="" src="./images/logo/logo2.png" alt="logo" />
+        <a href="/" class="  font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-[#FA5252] to-[#DD2476]">
+            <!-- <img class="" src="./images/logo/logo2.png" alt="logo" /> -->
+            <i class="fa-sharp fa-solid fa-code"></i> DevAhmad
         </a>
+
         <div class="flex items-center">
             <!-- dark and light mode toggle -->
             <button id="theme-toggle" type="button" class="dark-light-btn">
